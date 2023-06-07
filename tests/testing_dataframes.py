6b9be37ac57f4a3285a382f5171be8f7d0a0363c
Salvatore Architetto <salvatore.architetto@lovevery.com>
2023-06-06 22:48:04 -0400

    More refactoring + new tests

---
+++ tests/testing_dataframes.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+class TestingDataframes:
+    test_ids = [1, 2, 3]
+    test_names = ["premier_leage", "serie_a", "la_liga"]
+    test_names_none = [None, None, None]
+    test_times = ["2023-05-29", "2023-05-31", "2000-01-01"]
+    test_times_none = [None, None, None]
+    test_dataframe = pd.DataFrame(zip(test_ids, test_names, test_times),
+                                  columns=["id", "division", "date"])
+    test_dataframe_missing_cols = pd.DataFrame(test_ids, columns=["id"])
+    cleaned_dataframe = pd.DataFrame(zip(test_ids, test_names_none, test_times_none),
+                                     columns=["id", "division", "date"])
+    uppercase_dataframe = pd.DataFrame(zip(test_ids, test_names_none, test_times_none),
+                                       columns=["id", "date", "division"])
+    unnamed_dataframe = pd.DataFrame(zip(test_ids, test_names_none, test_times_none,
+                                         test_times_none, test_times_none),
+                                     columns=["id", "date", "division", "Unnamed", "unnamed"])
+    empty_dataframe = pd.DataFrame(zip(test_ids, test_names_none, test_times_none),
+                                   columns=["id", "date", "division"])
